898bc64a86954ff69d3d0fd05a0b9dfc9e247b94
Ana Laura Rosas <peppercat3120@gmail.com>
2026-09-15 14:29:57 -0600

    Enhancement(UI): Add custom icon and splash screen

---
+++ android/app/src/main/java/com/tsoftpokedex/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.tsoftpokedex
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+
+    window.decorView.post {
+      startActivity(Intent(this, MainActivity::class.java))
+      finish()
+    }
+  }
+}
